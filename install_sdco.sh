359b03d6cc1fc736fd74323493d49d9c0c52ed97
novarobot <88540431+novarobot@users.noreply.github.com>
2023-03-21 23:11:49 +0100

    Update install_sdco.sh

--- install_sdco.sh
+++ install_sdco.sh
@@ -11,12 +11,18 @@ mkdir -p outputs/txt2img-samples/samples
 #conda env create -f environment-cpuonly.yaml
 #conda activate sdco
 
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
 pip install pynvml gradio keras-unet fairseq basicsr facexlib
 pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
 pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
 pip install -e git+https://github.com/TencentARC/GFPGAN#egg=GFPGAN
 pip install -e git+https://github.com/xinntao/Real-ESRGAN#egg=realesrgan
 #pip install -e git+https://github.com/hlky/k-diffusion-sd#egg=k_diffusion
+
+pip install chardet
+pip install --force-reinstall charset-normalizer==3.1.0
+pip install git+https://github.com/crowsonkb/k-diffusion/
 
 #cp sd-v1-4.ckpt models/ldm/stable-diffusion-v1/model.ckpt
 #rm sd-v1-4.ckpt
